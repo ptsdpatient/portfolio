49fd3e876887f6d2b5bea9c5b574ba448377f913
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-02-11 13:12:43 +0530

    yt-bg

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,7 +6,7 @@
 <div class="fixed bg-black w-full flex flex-row justify-around px-20 bg-opacity-50 backdrop-blur-sm " style="height:4rem;z-index:9999;user-select:none;">
 	<a class="text-white text-xl my-auto cursor-pointer" href="#linkedin"><div class="h-full p-4 hover:bg-blue-700 rounded" style="font-family:headerFont;">Linked In</div></a>
 	<a class="text-white text-xl my-auto cursor-pointer" href="#github"><div class="h-full p-4 hover:bg-pink-700 rounded" style="font-family:headerFont">Github</div></a>
-	<a class="text-white text-xl my-auto cursor-pointer" href="#youtube"><div class="h-full p-4 hover:bg-red-700 rounded" style="font-family:headerFont">Youtube</div></a>
+	<a class="text-white text-xl my-auto cursor-pointer" href="#youtube"><div class="h-full p-4 hover:bg-red-600 rounded" style="font-family:headerFont">Youtube</div></a>
 	<a class="text-white text-2xl my-auto cursor-pointer" href="#tanishqdhote"><div class="rounded h-full p-4 hover:bg-white hover:text-black" style="font-family:headerFont">Tanishq Dhote</div></a>	
 	<a class="text-white text-xl my-auto cursor-pointer" href="#instagram"><div id="instaIcon" class="rounded h-full p-4" style="font-family:headerFont">Instagram</div></a>
 	<a class="text-white text-xl my-auto cursor-pointer" href="#itchio"><div class="h-full p-4 hover:bg-red-500 rounded" style="font-family:headerFont">Itch.io</div></a>
