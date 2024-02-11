5317d1773fab2d616a54017daba46936c37cb7ac
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-02-11 22:58:20 +0530

    Update Page1.svelte

--- src/routes/Page1.svelte
+++ src/routes/Page1.svelte
@@ -46,7 +46,7 @@
     <div class="w-full mx-auto flex flex-col md:flex-row md:justify-between pt-20 md:pt-20">
         
         <div class="w-4/5 md:w-2/5 bg-blue flex mx-auto justify-around " ><img src={standing}  alt="myself standing"></div>
-        <div class="w-3/5 px-10 mx-auto flex flex-col">
+        <div class="w-full md:w-3/5 px-10 mx-auto flex flex-col">
             <p class="text-{mobile?'2xl':'5xl'} mx-auto" style="font-family:titleFont;text:center;align:center"> Hello there👋</p>
             <p class="text-{mobile?'sm':'2xl'}" style="{mobile?'font-size:xl':''};font-family:data;align:center">Myself Tanishq Dhote, I have many interests, I am a meme page admin, a game developer, a web developer, a video game enthusiast and a script writer.</p>
             <p class="text-{mobile?'sm':'2xl'} mt-{mobile?'2':'10'}" style="{mobile?'font-size:xl':''};font-family:data;align:center">I am well versed with Svelte stack, MERN stack, Java(libGDX) for making crossplatform games and Python for automation, datascience and backend. </p>
