64b62170825214f5c5cdc0855b4f39e02fbf1f91
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-02-11 17:06:18 +0530

    Update Page1.svelte

--- src/routes/Page1.svelte
+++ src/routes/Page1.svelte
@@ -12,10 +12,10 @@
 </script>
 
 <div  id="tanishqdhote" class="w-full  justify-around" style="height:100vh;user-select:none">
-    <div class="w-full md:w-4/5 mx-auto flex flex-row md:justify-between pt-20 px-30" style="height:{mobile?'50vh':'100vh'}">
+    <div class="w-full md:w-4/5 mx-auto flex flex-col md:flex-row md:justify-between pt-20 md:pt-20 px-30" style="height:{mobile?'50vh':'100vh'}">
         
-        <div class="w-1/2 flex flex-row justify-around " style=""><img src={standing} alt="myself standing"></div>
-        <div class="w-{mobile?'full':'full'} flex flex-col">
+        <div class="w-1/2 flex mx-auto justify-around " ><img src={standing} style="height:{mobile?'50vh':'100vh'};" alt="myself standing"></div>
+        <div class="w-{mobile?'4/5 mx-auto':'full'} flex flex-col">
             <p class="text-{mobile?'2xl':'5xl'}" style="font-family:titleFont;align:center"> Hello there👋</p>
             <p class="text-{mobile?'':'xl'}" style="{mobile?'font-size:10px':''};font-family:data;align:center">Myself Tanishq Dhote, I have many interests, I am a meme page admin, a game developer, a web developer, a video game enthusiast and a script writer.</p>
             <p class="text-{mobile?'':'xl'} mt-{mobile?'2':'10'}" style="{mobile?'font-size:10px':''};font-family:data;align:center">I am well versed with Svelte stack, MERN stack, Java(libGDX) for making crossplatform games and Python for automation, datascience and backend. </p>
