306778b366f53a124df88339617c15bc533cd94c
Tanishq Dhote <tanishqbakka1@gmail.com>
2024-03-06 00:45:15 +0530

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -47,7 +47,7 @@ import {onMount} from 'svelte'
 <style>
 	@font-face {
     font-family: 'headerFont';
-    src: url('$lib/fonts/font-.ttf');
+    src: url('$lib/fonts/font-1.ttf');
   }
 
   #instaIcon:hover{
